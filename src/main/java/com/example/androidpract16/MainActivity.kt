7272cbd8c18562efadc8d30edee2b8c653f54010
Захар Голуб <pahargolub2@gmail.com>
2024-11-13 23:56:12 +0300

    1st prototype

--- src/main/java/com/example/androidpract16/MainActivity.kt
+++ src/main/java/com/example/androidpract16/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.androidpract16
 
 import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var theme:Int?=R.style.AppThemePrimary
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        installNewTheme()
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -19,13 +29,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 
-    fun onClickThemeSwitch(view: View):Unit{
-        val switchRef:Switch=view as Switch
-        if(switchRef.isChecked)
-            setTheme(R.style.AppThemePrimary)
-        else
-            setTheme(R.style.AppThemeSecondary)
+        if(theme!=null)
+            findViewById<Switch>(R.id.switch1).isChecked=theme!=R.style.AppThemeSecondary
+
+
+        findViewById<Switch>(R.id.switch1).setOnCheckedChangeListener{_, isChecked ->
+            val intent: Intent =Intent(this@MainActivity,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+
+
+            if (theme == null) {
+                this.theme=R.style.AppThemePrimary
+            }
+            if(theme==R.style.AppThemePrimary)
+                this.theme=R.style.AppThemeSecondary
+            else
+                this.theme=R.style.AppThemePrimary
+
+            intent.putExtra("theme",theme)
+
+            startActivity(intent)
+        }
+    }
+    fun installNewTheme(){
+        val arguments:Bundle?=intent.extras
+        if(arguments!=null){
+            theme=arguments.get("theme")as Int
+
+            if(theme!=null)
+                setTheme(theme!!)
+        }
     }
 }
