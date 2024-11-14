4669b609a8b5a44c9f820e4ffe62a19cd132ee5d
ClassZak <156465942+ClassZak@users.noreply.github.com>
2024-11-14 09:51:14 +0300

    Update files

--- src/main/java/com/example/androidpract16/MainActivity.kt
+++ src/main/java/com/example/androidpract16/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Switch
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -14,45 +15,56 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    var theme:Int?=R.style.AppThemePrimary
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        try {
+            super.onCreate(savedInstanceState)
 
 
-        installNewTheme()
+            //installNewTheme()
 
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        if(theme!=null)
-            findViewById<Switch>(R.id.switch1).isChecked=theme!=R.style.AppThemeSecondary
-
-
-        findViewById<Switch>(R.id.switch1).setOnCheckedChangeListener{_, isChecked ->
-            val intent: Intent =Intent(this@MainActivity,MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-
-
-            if (theme == null) {
-                this.theme=R.style.AppThemePrimary
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_main)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
             }
-            if(theme==R.style.AppThemePrimary)
-                this.theme=R.style.AppThemeSecondary
-            else
-                this.theme=R.style.AppThemePrimary
 
-            intent.putExtra("theme",theme)
+            findViewById<Switch>(R.id.switch1).setOnCheckedChangeListener{_, isChecked ->
+                if(isChecked)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                else
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
 
-            startActivity(intent)
+            /*if(theme!=null)
+                findViewById<Switch>(R.id.switch1).isChecked=theme!=R.style.AppThemeSecondary_Light
+
+
+            findViewById<Switch>(R.id.switch1).setOnCheckedChangeListener{_, isChecked ->
+                val intent: Intent =Intent(this@MainActivity,MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+
+
+                if (theme == null) {
+                    this.theme=R.style.AppThemePrimary_Light
+                }
+                if(theme==R.style.AppThemePrimary_Light)
+                    this.theme=R.style.AppThemeSecondary_Light
+                else
+                    this.theme=R.style.AppThemePrimary_Light
+
+                intent.putExtra("theme",theme)
+
+                startActivity(intent)
+            }*/
+        }
+        catch (e:Exception){
+            val message:Toast=Toast.makeText(this,"Ошибка\n"+e.message,Toast.LENGTH_LONG)
+            message.show()
         }
     }
-    fun installNewTheme(){
+    /*fun installNewTheme(){
         val arguments:Bundle?=intent.extras
         if(arguments!=null){
             theme=arguments.get("theme")as Int
@@ -60,5 +72,5 @@ class MainActivity : AppCompatActivity() {
             if(theme!=null)
                 setTheme(theme!!)
         }
-    }
+    }*/
 }
